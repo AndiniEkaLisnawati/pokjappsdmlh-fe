b2c1ceed35ccb3d65f823747e6b22cde03ecd066
AndiniEkaLisnawati <andiniekalisnawatililis2@gmail.com>
2025-08-12 11:48:43 +0700

    feat: add registration page with form handling and API integration

---
+++ src/app/register/page.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import { useState } from "react";
+import AuthForm from "@/components/auth/AuthForm";
+import Label from "@/components/auth/Label";
+import Input from "@/components/auth/Input";
+import Heading from "@/components/auth/Heading";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+
+export default function RegisterPage() {
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:3000/api/users", {
+        fullname,
+        email,
+        password,
+      });
+
+      toast("Register successfully!", {
+        description: "Akun berhasil dibuat",
+      });
+
+      const { fullname: name, role } = res.data;
+      localStorage.setItem("role", role);
+      localStorage.setItem("fullname", name);
+
+      router.push("/");
+    } catch (err) {
+      console.error({ message: "Gagal Register!", err });
+      toast("Register Failed!", {
+        description: "Please check your input fields",
+      });
+    }
+  };
+
+  return (
+    <>
+      <AuthForm btnType="submit" btnText="Register" onsubmit={handleSubmit}>
+        <Heading
+          title="Register Page"
+          description="Please create your acc first"
+        />
+
+        <div className="mb-6">
+          <Label text="Fullname" />
+          <Input
+            type="text"
+            placeholder="John doe"
+            value={fullname}
+            onChange={(e) => setFullname(e.target.value)}
+          />
+        </div>
+
+        <div className="mb-6">
+          <Label text="Email" />
+          <Input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter your Email"
+          />
+        </div>
+
+        <div className="mb-6">
+          <Label text="Password" />
+          <Input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter your Password"
+          />
+        </div>
+      </AuthForm>
+    </>
+  );
+}
